f466166e237fec49543c4ff890089cf7e1be9218
zengzhe <Zeng_Zhe@outlook.com>
2024-11-04 15:19:11 +0800

    feat: update

--- app.vue
+++ app.vue
@@ -1,20 +1,4 @@
 <template>
-  <div class="a">
-    <Button variant="secondary" class="relative h-full w-full" size="sm">
-      123
-    </Button>
-    <div>pos: {{ x }}, {{ y }}</div>
-  </div>
+  <NuxtWelcome />
 </template>
-<script setup lang="ts">
-const { x, y } = useMouse()
-_get(x, '123')
-</script>
-<style lang="scss" scoped>
-.a {
-  .b {
-    position: relative;
-    width: 100%;
-  }
-}
-</style>
+<script setup lang="ts"></script>
